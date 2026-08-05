8cf9897105cde80b085394ddaeeade22ee8f0a0a
Lucas Gomes da Cunha <117191728+LucasgcTi@users.noreply.github.com>
2026-08-05 20:11:58 -0300

    Add files via upload

---
+++ Aula03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main (void)
+{
+	cout << "Ola, Mundo!" << std::endl;
+	
+	cout << "Tecle <Enter> para encerrar... ";
+	cin.get();
+	
+	return 0;
+}
